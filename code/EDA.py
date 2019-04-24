7b7fa1a448e8b098a0547e491f754ce6d0439bf6
Ryan Miller <rm.miller83@gmail.com>
2019-04-24 13:23:00 -0400

    Initial commit

---
+++ code/EDA.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import graphfuncs as gf
+
+df = pd.read_csv('OnlineNewsPopularity.csv')
+
+timedelta_col = [1]
+word_cols = [2, 3, 4, 5, 6, 11]
+media_cols = [7, 8, 9, 10]
+cat_cols = [13, 14, 15, 16, 17, 18]
+keyword_cols = [12, 19, 20, 21, 22, 23, 24, 25, 26, 27]
+ref_cols = [28, 29, 30]
+day_cols = [31, 32, 33, 34, 35, 36, 37] #sat 36 and sun 37 cols are empty
+lda_cols = [39, 40, 41, 42, 43]
+subj_cols = [44, 56, 58]
+polar_cols = [45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 57, 59]
+share_col = [60]
+
+
+#### Strip white space from column names and add log shares
+df.columns = df.columns.str.strip()
+df['logshares'] = np.log(df['shares'])
+logshare_col = [df.columns.get_loc('logshares')]
+                
+##### Plot timedelta column in histogram
+#plt.figure()
+#g = sns.distplot(df['timedelta'], bins=30, kde=False)
+#plt.show()
+gf.get_dist_graph(df, 'timedelta')
+
+##### Make scatter plot of timedelta and log shares
+plt.figure()
+g = sns.regplot(x=df.loc[df['timedelta']<30, 'timedelta'], y=df.loc[df['timedelta']<30, 'logshares'])
+plt.show()
+
+##### Make scatter plot of timedelta and log shares
+plt.figure()
+g = sns.regplot(x=df['timedelta'], y=df['logshares'])
+plt.show()
+
+#####Calculate correlation between time since publ and log shares
+timecorr = np.corrcoef(df.iloc[:,timedelta_col], df.iloc[:,logshare_col], rowvar=False)
+print(timecorr)
+
+
+#####Calculate correlation of word_cols with log shares correlation matrix
+corrcols = word_cols + logshare_col
+#wordcorr = np.corrcoef(df.iloc[:, corrcols], rowvar=False)
+#plt.figure()
+#flatvals = wordcorr.flatten()
+#flatvals.sort()
+#sns.heatmap(wordcorr, vmax=flatvals[-(len(wordcorr)+1)], vmin=flatvals[0], xticklabels=df.columns[corrcols], yticklabels=df.columns[corrcols])
+gf.get_corr_heat_map(df.iloc[:, corrcols])
+
+#####Calculate correlation between content (links, videos) and log shares
+corrcols = media_cols + logshare_col
+#mediacorr = np.corrcoef(df.iloc[:, corrcols], rowvar=False)
+#plt.figure()
+#flatvals = mediacorr.flatten()
+#flatvals.sort()
+gf.get_corr_heat_map(df.iloc[:, corrcols])
+
+####Calculate shares by category in boxplot
+#for each in category columns, make entry in new column
+for cat in cat_cols:
+    catrows = df.iloc[:,cat]==1
+    df.loc[catrows,'category'] = df.columns[cat].split('_')[-1]
+    
+#plot the distributions of shares by category in box plot
+#df['logshares'] = np.log(df.iloc[:,share_col[0]])
+plt.figure()
+#sns.catplot(x=df.loc[df['category'].notnull(),'category'], y=df[df['category'].notnull()].iloc[:,share_col[0]], kind='violin')
+g = sns.catplot(x='category', y='logshares', data=df, kind='box')
+g.set_xticklabels(rotation=30)
+plt.show()
+
+#####Calculate shares by day of week in boxplot
+#for each in category columns, make entry in new column
+for day in day_cols:
+    dayrows = df.iloc[:,day]==1
+    df.loc[dayrows,'dayofweek'] = df.columns[day].split('_')[-1]
+    
+#plot the distributions of shares in box plot by day of week
+#df['logshares'] = np.log(df.iloc[:,share_col[0]])
+plt.figure()
+#sns.catplot(x=df.loc[df['category'].notnull(),'category'], y=df[df['category'].notnull()].iloc[:,share_col[0]], kind='violin')
+g = sns.catplot(x='dayofweek', y='logshares', data=df, kind='box')
+g.set_xticklabels(rotation=30)
+g.set()
+plt.show()
+
+#### Plot log shares grouped by day and category
+plt.figure()
+g = sns.barplot(x='dayofweek', y='logshares', hue='category', data=df)
+g.set_ylim(7)
+
+#####Calculate correlation of lda_cols with log shares correlation matrix
+corrcols = lda_cols + logshare_col
+gf.get_corr_heat_map(df.iloc[:, corrcols])
+
+#####Calculate correlation of polar_cols with log shares correlation matrix
+corrcols = polar_cols + logshare_col
+gf.get_corr_heat_map(df.iloc[:, corrcols])
+
+#####Calculate correlation of subj_cols with log shares correlation matrix
+corrcols = subj_cols + logshare_col
+gf.get_corr_heat_map(df.iloc[:, corrcols])
+
+#### shares by LDA topic and category
+df1 = df[['LDA_00','LDA_01','LDA_02','LDA_03','LDA_04','category','logshares']]
+df2 = pd.melt(df1, id_vars=['category','logshares'])
+plt.figure()
+sns.barplot(x='category', y='value', hue='variable', data=df2)
---
+++ code/capstone1bgm.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 08:27:58 2019
+
+@author: ryanmiller
+"""
+
+import lightgbm as lgb
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+import shap
+import matplotlib.pyplot as plt
+
+#%% Prepare dataset and parameters
+df = pd.read_csv('OnlineNewsPopularity.csv')
+
+#### Strip white space from column names and add log shares
+df.columns = df.columns.str.strip()
+df['logshares'] = np.log(df['shares'])
+
+          
+#Drop url, shares, weekday_is_monday, is_weekend
+df = df.drop(['shares', 'url', 'weekday_is_monday', 'is_weekend'], axis=1)
+
+#Get attribute names
+attributes = df.columns.drop('logshares')
+
+#Get target data and full attribute data
+y = df['logshares']
+X = df[attributes]
+
+
+#%% Do CV on full attribute set
+
+# specify your configurations as a dict
+params = {
+    'boosting': 'gbrt',
+    'objective': 'regression',
+    'n_estimators': 100
+}
+
+#Create datasets for lgbm
+lgb_train = lgb.Dataset(X, y)
+   
+#CV
+cv_results = lgb.cv(params,
+                    lgb_train, 
+                    nfold=5,
+                    metrics='l1',
+                    stratified=False)
+
+#grab error of last boosting round
+mae_total = cv_results['l1-mean'][-1]
+
+#%% Loop attributes, dropping one attribute at a time
+#maedict = {}
+#for a in attributes:
+#    #Print name of attribute
+#    print('Currently dropping: {}'.format(a))
+#    #Create training data by dropping attribute
+#    X1 = X.drop(a, axis=1)
+#    
+#    #Separate training and test data
+#    X_train, X_test, y_train, y_test = train_test_split(X1, y, test_size=0.2, random_state=21)
+#    
+#    #Create datasets for lgbm
+#    lgb_train = lgb.Dataset(X_train, y_train)
+#    lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
+#
+#    # train model
+#    gbm = lgb.train(params,
+#                    lgb_train,
+#                    valid_sets=lgb_eval)
+#    
+#    # predict
+#    y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration)
+#    # eval and add error to 
+#    maedict[a] = mean_absolute_error(y_test, y_pred)
+    
+#%% Loop drop attributes and do CV
+maedict = {}
+for a in attributes:
+    #Print name of attribute
+    print('Currently dropping: {}'.format(a))
+    #Create training data by dropping attribute
+    X1 = X.drop(a, axis=1)
+    
+    #Create datasets for lgbm
+    lgb_train = lgb.Dataset(X1, y)
+   
+    #CV
+    cv_results = lgb.cv(params,
+                        lgb_train, 
+                        nfold=5,
+                        metrics='l1',
+                        stratified=False)
+    
+    #add last boosting round error to dict
+    maedict[a] = [cv_results['l1-mean'][-1], 
+                  cv_results['l1-stdv'][-1],
+                  cv_results['l1-mean'][-1] - mae_total]
+
+
+#%% Create dataframe with error data
+df_mae = pd.DataFrame.from_dict(maedict, 
+                            orient='index', 
+                            columns=['mae_mean_without',
+                                     'mae_std_without',
+                                     'mae_diff_from_full'])
+
+#%% Plot va
+plotdata = df_mae.sort_values('mae_diff_from_full')
+fig, ax = plt.subplots()
+ax.bar(x=plotdata.index, height=plotdata.mae_diff_from_full)
+ax.set_xticklabels(labels=plotdata.index, rotation=60)
+
+
+#%%Do PCA on variables whose inclusion decrease error by <.0005
+#Grab data for weak variables
+df_weak = df[df_mae[df_mae['mae_diff_from_full']<.0005].index]
+#Scale data for PCA
+df_weak_scaled = preprocessing.scale(df_weak)
+#Do PCA
+pca = PCA()
+pca.fit(df_weak_scaled)
+pca_data = pca.transform(df_weak_scaled)
+#Generate scree plot
+per_var = np.round(pca.explained_variance_ratio_*100, decimals=1)
+labels = ['PC' + str(x) for x in range(1, len(per_var)+1)]
+plt.bar(x=range(1,len(per_var)+1), height=per_var, tick_label=labels)
+plt.ylabel('Percentage of Explained Variance')
+plt.xlabel('Principal Component')
+plt.title('Scree Plot')
+plt.show()
+#Plot loading scores for first and 2nd variables
+pca_df = pd.DataFrame(pca_data, columns=labels)
+plt.scatter(pca_df.PC1, pca_df.PC2)
+plt.title('PCA Graph for first two PC')
+plt.xlabel('PC1 - {0}%'.format(per_var[0]))
+plt.ylabel('PC2 - {0}%'.format(per_var[1]))
+plt.show()
+#Look at variables that contributed most to PC1
+loading_scores = pd.Series(pca.components_[0], index=df_weak.columns)
+sorted_loading_scores = loading_scores.abs().sort_values(ascending=False)
+top_10_vars = sorted_loading_scores[0:10].index.values
+print(loading_scores[top_10_vars])
+
+#%% Tune hyperparamters
+
+#Create lgb regressor model
+model = lgb.LGBMRegressor(boosting_type = 'gbrt',
+                          objective = 'regression',
+                          learning_rate = 0.01,
+                          colsample_bytree = .7,
+                          max_depth = 6,
+                          min_child_samples=60,
+                          num_leaves = 60,
+                          #n_estimators = 1000,
+                          random_state = 42,
+                          )
+   
+#parameters to optimize
+params_opt = {
+        #'max_depth': range(4, 9, 2),
+        'n_estimators': [2000],
+        #'min_child_samples': range(20, 101, 10)
+        #'num_leaves': range(20, 101, 20)
+        #'colsample_bytree': np.arange(.1, 1.01, .1)
+        
+        }
+
+#Run grid search
+gridsearch = GridSearchCV(estimator = model, 
+                          param_grid = params_opt,
+                          cv = 5,
+                          scoring = 'neg_mean_absolute_error')
+gridsearch.fit(X, y)
+
+#Print best params
+print("The best params are: {}".format(gridsearch.best_params_))
+
+
+#%% Plot shap values and importances
+## Create object that can calculate shap values
+#explainer = shap.TreeExplainer(gbm)
+#
+## calculate shap values. This is what we will plot.
+## Calculate shap_values for all of val_X rather than a single row, to have more data for plot.
+#shap_values = explainer.shap_values(X_test)
+#
+## Make shap plot
+#shap.summary_plot(shap_values, X_test)
+#
+#print('Plotting feature importances...')
+#ax = lgb.plot_importance(gbm, max_num_features=20)
+#plt.show()
+#
+#ax = lgb.plot_importance(gbm, max_num_features=20, importance_type='gain')
+#plt.show()
+
+#%% Testing ground to figure out differences in output
+
+#Create lgb regressor model
+model = lgb.LGBMRegressor(boosting_type = 'gbrt',
+                          objective = 'regression_l1',
+                          random_state = 42,
+                          n_estimators = 500,
+                          learning_rate = .01
+                          )
+   
+#parameters to optimize
+params_opt = {
+        #'max_depth': range(4, 9, 2),
+        #'n_estimators': [100],
+        #'min_child_samples': range(20, 101, 10)
+        #'num_leaves': range(20, 101, 20)
+        #'colsample_bytree': np.arange(.1, 1.01, .1)
+        
+        }
+
+#Run grid search
+gridsearch = GridSearchCV(estimator = model, 
+                          param_grid = params_opt,
+                          cv = 5,
+                          scoring = 'neg_mean_absolute_error')
+gridsearch.fit(X, y)
+
+#Print best score
+print("The best score for GridSearch is: {}".format(gridsearch.best_score_))
+
+##### Try lgb.cv
+# specify your configurations as a dict
+params = {
+    'boosting': 'gbrt',
+    'objective': 'regression_l1',
+    'num_boost_round': 500,
+    'random_state': 42,
+    
+    'max_depth': -1,
+    'num_leaves': 31,
+    'subsample': 1,
+    'colsample_bytree': 1,
+    'learning_rate': .01,
+    'min_child_weight': .001
+}
+
+#Create datasets for lgbm
+lgb_train = lgb.Dataset(X, y)
+   
+#CV
+cv_results = lgb.cv(params,
+                    lgb_train, 
+                    nfold=5,
+                    metrics='l1',
+                    stratified=False)
+
+#grab error of last boosting round
+print("The best score for lgb.cv is: {}".format(cv_results['l1-mean'][-1]))
